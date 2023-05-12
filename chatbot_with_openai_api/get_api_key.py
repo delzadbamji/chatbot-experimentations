0d3761b3729ffcfa615abd46ea63e0e52cd56fbf
delzadbamji <delzadbamji@gmail.com>
2023-05-12 16:33:48 -0700

    update code, test, and gitignore

--- chatbot_with_openai_api/get_api_key.py
+++ chatbot_with_openai_api/get_api_key.py
@@ -1,14 +1,5 @@
 
-# test_api_key_file = "chatbot-with-openai-api/apikey.txt"
+import os
 
-def get_api(api_key_file):
-  api_key = ""
-  try:
-    with open (api_key_file,"r") as api_file:
-      api_key = api_file.read()
-  except:
-    api_key = None
-
-  return api_key
-
-# get_api(test_api_key_file)
+def get_api():
+  return os.environ['api_key'] if os.environ['api_key'] is not None else None
--- chatbot_with_openai_api/tests/test_apikey.py
+++ chatbot_with_openai_api/tests/test_apikey.py
@@ -1,10 +1,9 @@
 import pytest
 from chatbot_with_openai_api.get_api_key import get_api
+import os
 
 def test_api_key():
-  api_key_file = "chatbot-with-openai-api/apikey.txt"
-  test_key = ""
-  with open(api_key_file) as file:
-    test_key = file.read()
+  assert get_api() == os.environ['api_key']
 
-  assert get_api(api_key_file) == test_key
+def test_api_key_None():
+  assert get_api() == os.environ['api_key2']
